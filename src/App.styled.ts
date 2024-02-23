75ae4371c36c6cc675c8cfd8a4a5d76f2925fae7
Dion D. Prawira <dion.dirza@gmail.com>
2024-02-23 15:24:24 +0700

    feat: use layout

--- src/App.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const AppContainer = styled.div`
-  display: flex;
-
-  > div:first-of-type {
-    flex-grow: 1;
-  }
-`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Layout } from 'antd';
 import type { FC } from 'react';
 import { RentalForm } from './components/Form';
 import { Map } from './components/Map';
@@ -7,7 +8,6 @@ import { useRentalCar } from './hooks';
 import { cars } from './seed';
 
 import './App.css';
-import { AppContainer } from './App.styled';
 
 export const App: FC = () => {
   const {
@@ -23,21 +23,25 @@ export const App: FC = () => {
 
   return (
     <ThemeProvider>
-      <AppContainer>
-        <Map markers={carLocations} onClick={onMapClick} />
-        <div>
-          <RentalCarTable dataSource={rentalCars} selectedKey={selectedCar?.id} onRowClick={onTableRowClick} />
-          {selectedCar && (
-            <RentalForm
-              available={selectedCar.available}
-              name={selectedCar.bookedBy}
-              hasReturnLocation={rentedCarHasReturnLocation}
-              onRentClick={onFormRentClick}
-              onReturnClick={onFormReturnClick}
-            />
-          )}
-        </div>
-      </AppContainer>
+      <Layout hasSider>
+        <Layout.Content>
+          <Map markers={carLocations} onClick={onMapClick} />
+        </Layout.Content>
+        <Layout.Sider style={{ padding: 16 }} width="40%">
+          <div>
+            <RentalCarTable dataSource={rentalCars} selectedKey={selectedCar?.id} onRowClick={onTableRowClick} />
+            {selectedCar && (
+              <RentalForm
+                available={selectedCar.available}
+                name={selectedCar.bookedBy}
+                hasReturnLocation={rentedCarHasReturnLocation}
+                onRentClick={onFormRentClick}
+                onReturnClick={onFormReturnClick}
+              />
+            )}
+          </div>
+        </Layout.Sider>
+      </Layout>
     </ThemeProvider>
   );
 };
